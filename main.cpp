29d993e86dc79977ba9aa8f64d7587b03ab2f81a
kseniyasvidinskaya <kseniya.angarsk@mail.ru>
2026-04-08 23:03:29 +0700

    добавлена программа для треугольника

--- main.cpp
+++ main.cpp
@@ -1,57 +1,54 @@
 #include <iostream>
-using namespace std;
+#include <cmath>
 int main() {
-	cout << "Figure: trapezoid darya" << endl;
-	int a, b, c, d, h;
+    double a, b, c;
+    std::cout << "Shape: triangle" << std::endl;
+    // Ввод стороны a
+    do {
+        std::cout << "Enter a > 0: ";
+        std::cin >> a;
+        if (std::cin.fail() || a <= 0) {
+            std::cout << "must be positive number" << std::endl;
+        }
+    } while (a <= 0);
+    // Ввод стороны b
+    do {
+        std::cout << "Enter b > 0: ";
+        std::cin >> b;
+        if (std::cin.fail() || b <= 0) {
+            std::cout << "must be positive number" << std::endl;
+        }
+    } while (b <= 0);
+    // Ввод стороны c
+    do {
+        std::cout << "Enter c > 0: ";
+        std::cin >> c;
+        if (std::cin.fail() || c <= 0) {
+            std::cout << "must be positive number" << std::endl;
+        }
+    } while (c <= 0);
+    // Проверка существования треугольника
+    if (a + b <= c || a + c <= b || b + c <= a) {
+        std::cout << "\nSuch a triangle does not exist!" << std::endl;
+        return 1;
+    }
+    // Периметр
+    double perimeter = a + b + c;
+    // Площадь по формуле Герона
+    double p = perimeter / 2;  // полупериметр
+    double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    // Проверка на равнобедренность
+    bool Isosceles = (a == b) || (a == c) || (b == c);
 
-   	do {
-		cout << "a= ";
-		cin >> a;
-		if (a<=0) {
-			cout << "error\n";
-		}
-	} while (a<=0);
+    std::cout << "\nResults:" << std::endl;
+    std::cout << "Perimeter = " << perimeter << std::endl;
+    std::cout << "Area = " << s << std::endl;
+    
+    if (Isosceles) {
+        std::cout << "The triangle is isosceles" << std::endl;
+    } else {
+        std::cout << "The triangle is NOT isosceles" << std::endl;
+    }
 
-	do {
-		cout << "b= ";
-		cin >> b;
-		if (b<=0) {
-			cout << "error\n";
-		}
-	} while (b<=0);
-
-	do {
-		cout << "c= ";
-		cin >> c;
-		if (c<=0) {
-			cout << "error\n";
-		}
-	} while (c<=0);
-
-	do {
-		cout << "d= ";
-		cin >> d;
-		if (d<=0) {
-			cout << "error\n";
-		}
-	} while (d<=0);
-
-	do {
-		cout << "h= ";
-		cin >> h;
-		if (h<=0) {
-			cout << "error\n";
-		}
-	} while (h<=0);
-
-	int P = a+b+c+d;
-	double S = (a+b) / 2.0 * h;
-	double M = (a+b) / 2.0;
-
-	cout<<"Perimetr= " << P << endl;
-	cout<<"Area= " << S << endl;
-	cout << "Midline= " << M << endl;
-
-	return 0;
+    return 0;
 }
- 
